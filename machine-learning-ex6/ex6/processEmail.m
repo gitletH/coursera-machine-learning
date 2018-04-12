11ca304acb217f232e55af9097e99deac184b04e
Tianjiao Huang <maitian519@gmail.com>
2018-04-12 13:09:16 -0700

    ex6: finish

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -96,14 +96,13 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
+    N = size(vocabList, 1);
+    for i=1:N
+      if strcmp(str, vocabList{i})
+        word_indices = [word_indices; i];
+        break;
+      endif
+    endfor    
 
 
     % =============================================================
