880c9416bace60d6882ae83d12af78acb714a2e9
Tianjiao Huang <maitian519@gmail.com>
2018-04-12 12:36:25 -0700

    ex6: finish first half

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,31 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+value_to_try = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+best_C = C;
+best_sigma = sigma;
+best_error = inf;
 
+for i=1:8
+  for j=1:8
+    C = value_to_try(i);
+    sigma = value_to_try(j);
+    
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    pred = svmPredict(model, Xval);
+    error = mean(double(pred ~= yval));
+    
+    if error < best_error
+      best_error = error;
+      best_C = C;
+      best_sigma = sigma;
+    endif  
+  endfor
+endfor  
+
+C = best_C;
+sigma = best_sigma;
 
 
 
